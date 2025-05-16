afbcb56505df59865497abfc432845d1870c54c9
unknown <sabbirahmed.official1999@gmail.com>
2025-05-16 20:15:27 +0600

    map basic program

--- main.go
+++ main.go
@@ -20,4 +20,33 @@ func main(){
 	delete(fruits, "banana")
 
 	fmt.Println(fruits)
+
+	//Map থেকে ডাটা রিড করা
+	V := fruits["apple"]
+    fmt.Println(V) // Output: 10
+
+
+	// Map-এ কোনো key আছে কিনা চেক করা
+
+	 val, ok := fruits["banana"]
+        if ok {
+          fmt.Println("Found:",val)
+        } else {
+          fmt.Println("Not found")
+        }
+
+		//Map এর উপর loop চালানো
+
+		for key, value := range fruits {
+           fmt.Println(key, value)
+           }
+
+	
+	 //Map এর length জানা
+
+	   fmt.Println(len(fruits))
+	   
+
+
+
 }
